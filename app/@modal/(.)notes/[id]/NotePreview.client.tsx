58a3226da920b868db32bcb51f73b6c85d30caaa
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-02-26 21:42:09 +0200

    hot fix modal

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -1,15 +1,28 @@
 "use client";
 
 import Modal from "@/components/NotePreviewModal/NotePreviewModal";
+import { fetchNoteById } from "@/lib/api";
 import { Note } from "@/types/note";
-
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
 // import NotePreview from "./page";
 
-interface Props {
-  note: Note;
-}
+//:use Query
 
-const NoteModal = ({ note }: Props) => {
+const NoteModal = () => {
+  const { id } = useParams();
+  const {
+    data: note,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["note", { id: id }],
+    queryFn: () => fetchNoteById(id as string),
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error || !note) return <p>Some error..</p>;
+
   return (
     <Modal>
       <h2>{note.title}</h2>
--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,4 +1,10 @@
 import { fetchNoteById } from "@/lib/api";
+//: Libraries
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from "@tanstack/react-query";
 // import css from "./Modal.module.css";
 import NoteModal from "./NotePreview.client";
 type Props = {
@@ -7,10 +13,21 @@ type Props = {
 };
 
 const NotePreview = async ({ params }: Props) => {
-  const { id } = await params;
-  const note = await fetchNoteById(id);
+  const queryClient = new QueryClient();
 
-  return <NoteModal note={note} />;
+  const { id } = await params;
+  // const note = await fetchNoteById(id);
+
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteModal />;
+    </HydrationBoundary>
+  );
 };
 
 export default NotePreview;
